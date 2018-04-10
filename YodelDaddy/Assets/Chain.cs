68bc0ccf5c790670629c3d0471ad891244c5b5c9
lip ables <phillipables1@gmail.com>
2018-04-10 16:22:20 -0400

    Chain script public Transform for player and public static bool isFired set to false on start and in Update if not fired transform position to player position

---
+++ YodelDaddy/Assets/Chain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour {
+    public Transform target;
+
+    public static bool isFired;
+	void Start () {
+        isFired = false;
+	}
+	
+	void Update () {
+        if (isFired)
+        {
+
+        }
+        else
+        {
+            transform.position = target.position;
+        }
+	}
+}
