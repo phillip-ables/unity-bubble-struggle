236af4c1eee0a73070ab06060d4618ac33d1f810
lip ables <phillipables1@gmail.com>
2018-04-09 20:16:48 -0400

    add rigidbody player to script rigidbody game object, on FixedUpdate MovePosition of said rigidbody to position plus new vector 2 of movement times fixedDeltaTime and 0f, also deleted deltaTime multiplier when movement is set in on Update

--- YodelDaddy/Assets/BubbleTrouble/Scripts/Player.cs
+++ YodelDaddy/Assets/BubbleTrouble/Scripts/Player.cs
@@ -2,13 +2,16 @@
 
 public class Player : MonoBehaviour {
     public float speed = 4;
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
 
-    public float movement;
+    private float movement;
 
     void Update () {
         movement = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;  // I like it with deltaTime
 	}
 
-
+    void FixedUpdate()  // physics engine likes for us to update velocity in this
+    {
+        rb.MovePosition(rb.position + new Vector2 (movement * Time.fixedDeltaTime, 0f));
+    }
 }
