ae69ae2af5c165545cbf3a149d1b2fd80d3ccdfc
lip ables <phillipables1@gmail.com>
2018-04-11 08:18:07 -0400

    changed soooooo much stuff, hope i got it all back, fixed but -> OnTrigerEnder2D instead of collision and yeah, hope i fixed everything, definitly learned some things

--- YodelDaddy/Assets/Scenes/BubbleTrouble/Scripts/ChainCollision.cs
+++ YodelDaddy/Assets/Scenes/BubbleTrouble/Scripts/ChainCollision.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ChainCollision : MonoBehaviour {
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Collision");
         Chain.IsFired = false;
 
-        if(collision.collider.tag == "Ball")
+        if (collision.tag == "Ball")
         {
-            Debug.Log("It totally should work");
-            collision.collider.GetComponent<Ball>().Split();
+            collision.GetComponent<Ball>().Split();
         }
 
     }
